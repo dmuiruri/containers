f4a635537cbe39a65feab57eaffa22348b1e57ca
dennisn <denonjugush@gmail.com>
2019-01-06 19:30:43 +0200

    Initial docker configuration files

---
+++ ex4/sc.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash                                                                                                                                                  
+read website;
+sleep 3;
+curl http://$website;
